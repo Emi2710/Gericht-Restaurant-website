e94d02fd9f53e2c2596dedf54f5c143ede3884b0
Emi2710 <deadourfreedom@gmail.com>
2022-06-18 20:42:54 +0300

    header

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
 
+import { images } from '../../constants';
+import { SubHeading } from '../../components';
 import './Header.css';
 
 const Header = () => (
-  <div>
-    Header
+  <div className='app__header'>
+    <div className='app__wrapper-info'>
+      <SubHeading title="Chase the new flavor" />
+      <h1 className='app__header-h1 headtext__cormorant'>The Key To Fine Dining</h1>
+      <p className='app__header-paragraph p__opensans'>Sit tellus lobortis sed senectus vivamus molestie. Condimentum volutpat morbi facilisis quam scelerisque sapien. Et, penatibus aliquam amet tellus </p>
+      <button type="button" className='custom__button'>Explore Menu</button>
+    </div>
+    <div className='app__header-img'>
+      <img src={images.welcome} alt="header_img" />
+    </div>
   </div>
 );
 

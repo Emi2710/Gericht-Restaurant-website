7a19ee9c88070793e6d86f8dab65e70076c12726
Emi2710 <deadourfreedom@gmail.com>
2022-07-05 19:52:18 +0300

    back to top arrow

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import React, {useState, useEffect } from 'react';
+import React, { Fragment } from 'react';
 
 import { ParallaxProvider } from 'react-scroll-parallax';
 
 import { Routes, Route } from 'react-router-dom';
 
 import { Navbar } from './components';
-import Footer from './components/Footer/Footer'
+import Footer from './components/Footer/Footer';
+import Scroll from './components/Scroll/Scroll';
 
 import Home from './pages/Home/Home';
 import About from './pages/About/About';
@@ -18,18 +19,28 @@ const App = () => {
 
   return(
     <ParallaxProvider>
-      <div style={{backgroundColor:'#0C0C0C'}}>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="menu" element={<Menu />} />
-          <Route path="contact" element={<Contact />} />
-        </Routes>
-        <Footer />
+
+
+      <Fragment>
+        <div style={{backgroundColor:'#0C0C0C'}}>
+          <Navbar />
+          
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="menu" element={<Menu />} />
+            <Route path="contact" element={<Contact />} />
+          </Routes>
+
+          
+          <Footer />  
+          <Scroll />
+        </div> 
+      </Fragment>
+     
       
         
-      </div>    
+         
     </ParallaxProvider>
   
 )};
--- src/components/index.js
+++ src/components/index.js
@@ -1,9 +1,11 @@
 import SubHeading from './SubHeading/SubHeading';
 import MenuItem from './Menuitem/MenuItem';
 import Navbar from './Navbar/Navbar';
+import Scroll from './Scroll/Scroll';
 
 export {
   SubHeading,
   MenuItem,
   Navbar,
+  Scroll
 };
